9a770d716031947b2812765eba062f9293c4a3b7
Beau Spencer <bdspencer416@gmail.com>
2018-10-02 13:48:24 -0700

    Improved aesthetic of the React form component. #1

--- src/form.js
+++ src/form.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Form(props) {
   return (
-    <div className="card">
-      <div className="card-header">Create a Flash Card</div>
+    <div className="card mx-auto mt-5 text-center" style={{width: "18rem"}}>
+      <div className="card-header text-left">Create a Flash Card</div>
       <div className="card-body">
         <form>
-          <div className="form-group">
-            <label htmlFor="inputQuestion">Question</label>
-            <input type="text" id="questionInput" placeholder="Enter card question here"/>
+          <div className="form-group text-left">
+            <label htmlFor="inputQuestion" className="mr-2">Question:</label>
+            <input type="text" id="questionInput" size="28" placeholder="Enter card question here"/>
           </div>
-          <div className="form-group">
-            <label htmlFor="inputAnswer">Answer</label>
-            <input type="text" id="answerInput" placeholder="Enter card answer here"/>
+          <div className="form-group text-left">
+            <label htmlFor="inputAnswer" className="mr-2">Answer:</label>
+            <input type="text" id="answerInput" size="28" placeholder="Enter card answer here"/>
           </div>
           <button type="submit" className="btn btn-primary">Save</button>
         </form>
