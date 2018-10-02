1be57235c1d0c48636fa3562c53c2514fc8bc38f
Beau Spencer <bdspencer416@gmail.com>
2018-10-02 15:54:35 -0700

    adjusted styling of inputs to use boostrap form-control class and styled card with exterior styles object. #1

--- src/form.js
+++ src/form.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
 export default function Form(props) {
+  const style = {
+    card: {
+      width: '32rem'
+    }
+  }
   return (
-    <div className="card mx-auto mt-5 text-center" style={{width: "32rem"}}>
+    <div className="card mx-auto mt-5 text-center" style={style.card}>
       <div className="card-header text-left">Create a Flash Card</div>
       <div className="card-body">
         <form onSubmit={props.submit}>
           <div className="form-group text-left">
             <label htmlFor="inputQuestion" className="mr-2">Question:</label>
-            <input name="question" type="text" id="questionInput" size="56" placeholder="Enter card question here"/>
+            <input name="question" type="text" id="inputQuestion" className="form-control" placeholder="Enter card question here"/>
           </div>
           <div className="form-group text-left">
             <label htmlFor="inputAnswer" className="mr-3">Answer:</label>
-            <input type="text" id="answerInput" name="answer" size="56" placeholder="Enter card answer here"/>
+            <input type="text" id="inputAnswer" name="answer" className="form-control" placeholder="Enter card answer here"/>
           </div>
           <button type="submit" className="btn btn-primary">Save</button>
         </form>
