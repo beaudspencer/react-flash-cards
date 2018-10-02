3ab88e7c69130011ba5edf8a21df7d13f55e39fb
Beau Spencer <bdspencer416@gmail.com>
2018-10-02 15:21:55 -0700

    made form a little bigger to increase its appeal. #1

--- src/form.js
+++ src/form.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Form(props) {
   return (
-    <div className="card mx-auto mt-5 text-center" style={{width: "18rem"}}>
+    <div className="card mx-auto mt-5 text-center" style={{width: "32rem"}}>
       <div className="card-header text-left">Create a Flash Card</div>
       <div className="card-body">
         <form onSubmit={props.submit}>
           <div className="form-group text-left">
             <label htmlFor="inputQuestion" className="mr-2">Question:</label>
-            <input name="question" type="text" id="questionInput" size="28" placeholder="Enter card question here"/>
+            <input name="question" type="text" id="questionInput" size="48" placeholder="Enter card question here"/>
           </div>
           <div className="form-group text-left">
-            <label htmlFor="inputAnswer" className="mr-2">Answer:</label>
-            <input type="text" id="answerInput" name="answer" size="28" placeholder="Enter card answer here"/>
+            <label htmlFor="inputAnswer" className="mr-3">Answer:</label>
+            <input type="text" id="answerInput" name="answer" size="48" placeholder="Enter card answer here"/>
           </div>
           <button type="submit" className="btn btn-primary">Save</button>
         </form>
