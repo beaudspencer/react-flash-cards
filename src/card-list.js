4971a1c733a6ad38d0cfb18ab5f28b48b49a3178
Beau Spencer <bdspencer416@gmail.com>
2018-10-03 11:34:13 -0700

    Added a conditional React component that renders a phrase and button if user has no cards stored, and the button changes the hash. #2

--- src/card-list.js
+++ src/card-list.js
@@ -3,12 +3,20 @@ import Card from './card.js'
 
 export default function CardList(props) {
   return (
-    <div className="row ml-1">
-      <ul className="list-inline">
-        {props.cards.map((card, index) => {
-          return <Card key={index} card={card}/>
-        })}
-      </ul>
-    </div>
+    <React.Fragment>
+      {props.cards.length === 0 && <div className="text-center mt-5">
+        <h2>You have no cards saved</h2>
+        <a className="btn btn-primary mt-3" role="button" href="#new">Make a Card</a>
+      </div>}
+      {props.cards.length > 0 &&
+      <div className="row ml-1 mt-3">
+        <ul className="list-inline">
+          {props.cards.map((card, index) => {
+            return <Card key={index} card={card}/>
+          })}
+        </ul>
+      </div>
+      }
+    </React.Fragment>
   )
 }
