c0507cbd134c68e5a0e49a659b31dffbb764ff06
Beau Spencer <bdspencer416@gmail.com>
2018-10-03 10:39:20 -0700

    Added view property to app state that holds the location based on the hash and enhanced render to render Form when the view is set to new. #2

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import Form from './form.js'
 import Navbar from './navbar.js'
+import hash from './hash.js'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      cards: []
+      cards: [],
+      view: 'list'
     }
     this.addCard = this.addCard.bind(this)
   }
   componentDidMount() {
     window.addEventListener('hashchange', () => {
-      // going to do something soon
-      console.log('yo')
+      this.setState({view: hash.parse(location.hash)})
     })
   }
   addCard(card) {
@@ -25,7 +26,7 @@ export default class App extends React.Component {
     return (
       <React.Fragment>
         <Navbar/>
-        <Form submit={this.addCard}/>
+        {this.state.view === 'new' && <Form submit={this.addCard}/>}
       </React.Fragment>
     )
   }
