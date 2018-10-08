39cdc0f63e425d3981d9e6d42dfb3c77109c3d08
Beau Spencer <bdspencer416@gmail.com>
2018-10-08 13:34:00 -0700

    Fixed filter callback in FilterDrop component to properly render the unique topics of cards. #17

--- src/filter-dropdown.js
+++ src/filter-dropdown.js
@@ -10,15 +10,21 @@ export default class FilterDrop extends React.Component {
     this.setState({toggle: !this.state.toggle})
   }
   render() {
+    const topics = []
     const drop = this.state.toggle ? 'd-block' : 'd-none'
     return (
       <div className="dropdown">
         <button onClick={this.toggler} className="btn btn-primary mt-3 ml-1">Filter By:</button>
         <div>
           {this.props.cards
-            .filter((card, index, self) => self.indexOf(card) === index)
+            .filter((card) => {
+              if (!topics.includes(card.topic)) {
+                topics.push(card.topic)
+                return card
+              }
+            })
             .map((card) => {
-              return <button key={card.topic} onClick={this.props.sort} className={drop + ' btn btn-secondary m-1'}>{card.topic}</button>
+              return <button key={card.cardId} onClick={this.props.sort} className={drop + ' btn btn-secondary m-1'}>{card.topic}</button>
             })}
           <button onClick={this.props.sort} className={drop + ' btn btn-secondary m-1'}>none</button>
         </div>
