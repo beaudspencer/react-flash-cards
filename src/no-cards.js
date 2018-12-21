6ba1cb95681b02c128b3938f6209ab9376739b85
Beau Spencer <bdspencer416@gmail.com>
2018-12-21 00:06:15 +0000

    refactored code to have cleaner spacing. #21

--- src/no-cards.js
+++ src/no-cards.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function NoCards(props) {
   return (
     <div className="text-center mt-5">
-      <h2>You have no cards saved</h2>
+      <h2>
+        You have no cards saved
+      </h2>
       <a className="btn btn-primary mt-3" role="button" href="#new">
-    Make a Card
+        Make a Card
       </a>
     </div>
   )
